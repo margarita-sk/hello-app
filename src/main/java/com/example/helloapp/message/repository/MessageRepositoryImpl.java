c9efcbab9b3f8b7e8bf77c60a01301dc9889a84c
margarita-sk <mskokova2802@gmail.com>
2021-01-29 11:58:09 +0300

    dockerfile was removed, buildpack was used instead

--- src/main/java/com/example/helloapp/message/repository/MessageRepositoryImpl.java
+++ src/main/java/com/example/helloapp/message/repository/MessageRepositoryImpl.java
@@ -22,8 +22,13 @@ public class MessageRepositoryImpl implements MessageRepository {
   @Override
   public Collection<Message> findAll() {
     var criteriaBuilder = entityManager.getCriteriaBuilder();
+
+    // set the type of returned data
     var criteriaQuery = criteriaBuilder.createQuery(Message.class);
+
+    // set from where do we receive the data
     var root = criteriaQuery.from(Message.class);
+
     var typedQuery = entityManager.createQuery(criteriaQuery);
     var messages = typedQuery.getResultList();
     return messages;
